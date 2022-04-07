ad52f4450aaffff66739b180837f61ed9af87895
wanxuanh <wanxuan.ho@gmail.com>
2022-04-07 20:21:10 +0800

    fetch backend  menu done

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,24 +1,38 @@
 import { useEffect, useState } from "react";
 import urlcat from "urlcat"
 import { BACKEND } from "../utils/utils";
+import axios from "axios"
 
 
 export default function MenuPage() {
-     const [menu, setMenu] = useState([]);
-
+ const [menu, setMenu] = useState([]);
+    const menuName = [];
   useEffect(() => {
-    fetch(urlcat(BACKEND, "/menu"))
-      .then((response) => response.json())
-      .then((data) => setMenu(data));
+      console.log(BACKEND)
+     axios.get(urlcat(BACKEND, "/menu"))
+     .then((response) => {
+         console.log(response.data[0])
+         response.data.forEach((el) => {
+
+            menuName.push(el.name)
+         })
+         setMenu(menuName)
+
+        })
+      .catch ((error) => console.log(error))
   }, []);
 
     return (
         <>
         <h2>Menu</h2>
-         {menu.map((menus) => (
-           <li key={menus.name}>
+       <ul>
+        {menu.map((el) => (
+
+           <li>{el}
           </li>
         ))}
+      </ul>
+        
         </>
     )
 }
