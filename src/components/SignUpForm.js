5022946d997f4bb3d87d74bcf9b08fde21f27def
gnehsheng <gnehsheng@gmail.com>
2022-04-10 04:28:56 +0800

    tidying up signupform.js

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -36,27 +36,24 @@ export default function SignUpForm() {
         setUserDetails(newData)
     }
     return (
-        <div>
-            <form onSubmit={(e) => submitHandler(e)}>
-                <div className='form-inner'>
-                    <h2>SIGNUP</h2>
+        <form onSubmit={(e) => submitHandler(e)}>
+            <div className='form-inner'>
+                <h2>SIGNUP</h2>
 
-                    <div className='form-group'>
-                        <label htmlFor='name'>Name:</label>
-                        <input type='name' name='name' id='name' onChange={(e) => handleChange(e)} value={userDetails.name} />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='username'>Username:</label>
-                        <input type='username' name='username' id='username' onChange={(e) => handleChange(e)} value={userDetails.username} />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='password'>Password:</label>
-                        <input type='password' name='password' id='password' onChange={(e) => handleChange(e)} value={userDetails.password} />
-                    </div>
-                    <input type='submit' value='SIGNUP' />
+                <div className='form-group'>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='name' name='name' id='name' onChange={(e) => handleChange(e)} value={userDetails.name} />
                 </div>
-                <button onClick={navigate('/signup')}>Sign Up here!</button>
-            </form>
-        </div>
+                <div className='form-group'>
+                    <label htmlFor='username'>Username:</label>
+                    <input type='username' name='username' id='username' onChange={(e) => handleChange(e)} value={userDetails.username} />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password:</label>
+                    <input type='password' name='password' id='password' onChange={(e) => handleChange(e)} value={userDetails.password} />
+                </div>
+                <input type='submit' value='SIGNUP' />
+            </div>
+        </form>
     )
 }
