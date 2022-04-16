9d410580cfdf7961d94b38eedc885c035a088909
wanxuanh <wanxuan.ho@gmail.com>
2022-04-16 12:26:06 +0800

    merge

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+import { CookiesProvider } from 'react-cookie';
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-
-
+ReactDOM.render(
+	<CookiesProvider>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</CookiesProvider>,
+	document.getElementById('root')
+);
